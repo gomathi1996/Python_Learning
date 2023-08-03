820797c6ee3b8a9dce666a55f48abbab982550ac
gomathi1996 <gomathiece1996@gmail.com>
2023-08-03 13:28:19 +0530

    variables in python

---
+++ Basics/Variables.py
@@ -0,0 +1,14 @@
+print("hello world!!!")
+print(type('Hello World'))
+
+# Variables
+
+first_name="gomathi"
+last_name="manoharan"  #String
+Rank=50                #Integer
+Percentage=80.12       # Decimal i.e Float 
+print(first_name+" "+last_name)
+print(Rank)
+print(type(Rank))
+print(Percentage)
+print(type(Percentage))
